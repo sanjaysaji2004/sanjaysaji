a9a4a15abb2403d901df35d4acafba4cfa2dff22
sanjaysaji2004 <123439054+sanjaysaji2004@users.noreply.github.com>
2023-01-25 10:25:41 +0530

    Add files via upload

---
+++ happy number.py
@@ -0,0 +1,10 @@
+a=int(input("enter the number:"))
+def is_happy(n):
+    def get_sum_of_squares(num):
+        return sum([int(x)**2 for x in str(num)])
+    seen=set()
+    while n not in seen:
+        seen.add(n)
+        n=get_sum_of_squares(n)
+        return n==1
+print("the number is happy or not:",is_happy(a))
